9659112127f7dd8bced70cae025e8b62b62b274e
root <emploi.neveu@gmail.com>
2018-11-27 10:58:46 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <form action="user.php" method="post">
+    <p>prénom<input type="text" name="nom" value=""></p>
+    <p>nom<input type="text" name="nom" value=""></p>
+    <button type="submit" name="button">Valider</button>
+    </form>
+  </body>
+</html>
